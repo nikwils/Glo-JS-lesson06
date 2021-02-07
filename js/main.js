ecdd14f4366c9e5882de7909eecbb89f461eb605
Slivkin Eugene <slivkinebren1@gmail.com>
2021-02-07 14:37:15 +0300

    game ready

--- js/main.js
+++ js/main.js
@@ -4,14 +4,18 @@ const isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-const funTest = function() {
+
+function funTest() {
 
     let number = Math.floor(Math.random()*100);
     console.log(number);
+   
+    let attempts = 10;
 
     const funQuestion = function(){
-        
-        let answerOnQuestion = prompt('Угадай число от 1 до 100');
+
+        if(attempts > 0 ) {
+            let answerOnQuestion = prompt('Угадай число от 1 до 100');
         
         if (answerOnQuestion === null){
             alert('Игра окончена');
@@ -20,15 +24,31 @@ const funTest = function() {
             alert('Введите число');
             funQuestion();
         }else if( answerOnQuestion > number){
-            alert('Загаданное число меньше');
+            attempts --;
+            alert(`Загаданное число меньше, осталось попыток ${attempts}`);
             funQuestion();
-        } else if (answerOnQuestion < number) {
-            alert('Загаданное число больше');
+        }else if (answerOnQuestion < number) {
+            attempts --;
+            alert(`Загаданное число больше, осталось попыток ${attempts}`);
             funQuestion();
         }else {
-            return alert('Вы угадали загаданное число');
+            let b = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+            if (b === true) {
+                funTest();
+            } else {
+                return;
+            }
+        }
+        } else {
+            let a = confirm('Попытки закончились, хотите сыграть еще? ');
+            if (a === true) {
+                funTest();
+            } else {
+                return;
+            }
         }
     };
 
     funQuestion();
-}();
+};
+funTest();
